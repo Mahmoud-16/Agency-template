1e00afaf15fc67f05a9c446bb5eb765b1c123233
Mahmoud Gamal <mahmoudgamalawad10@gmail.com>
2022-04-21 10:28:13 +0200

    Landing Responsive

--- js/action.js
+++ js/action.js
@@ -1,5 +1,4 @@
 // Changing the landing background image every 10 seconds
-
 let landing = document.querySelector(".landing");
 
 let images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
@@ -10,3 +9,24 @@ setInterval(() => {
   // Every 10 sec the image will change
   landing.style.backgroundImage = `url(images/${images[randomNumber]})`;
 }, 10000);
+
+
+
+// Showing toggle menu once the icon is clicked
+let headerIcon = document.querySelector(".landing i");
+let toggleMenu = document.querySelector(".landing .links");
+
+headerIcon.addEventListener("click", function (e) {
+  if (toggleMenu.style.display === "none") {
+    toggleMenu.style.display = "block";
+  } else {
+    toggleMenu.style.display = "none";
+  }
+});
+
+// Close menu when clicking outside
+document.addEventListener("click", function (e) {
+  if (e.target !== headerIcon) {
+    toggleMenu.style.display = "none";
+  }
+});
